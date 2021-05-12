0cb313b0c462932024c2291c0ba4d22d1c0d45fe
Patricia <aaraujo.patricia@gmail.com>
2021-05-11 21:14:42 -0300

    Meu primeiro teste

---
+++ src/test/java/com/teste/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.teste;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
